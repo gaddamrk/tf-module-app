3a2f9b13398fc03b65fa890ae8472fe2b857fe50
gaddamrk <raj>
2023-09-05 21:42:08 +0530

    jr

--- main.tf
+++ main.tf
@@ -25,3 +25,47 @@ resource "aws_security_group" "main" {
     { Name = "${var.env}-${var.component}-security-group" }
   )
 }
+
+resource "aws_launch_template" "main" {
+  name_prefix   = "${var.env}-${var.component}-template"
+  image_id      = data.aws_ami.centos8.id
+  instance_type = var.instance_type
+}
+
+resource "aws_autoscaling_group" "asg" {
+  name                      = "${var.env}-${var.component}-asg"
+  max_size                  = var.max_size
+  min_size                  = var.min_size
+  desired_capacity          = var.desired_capacity
+  force_delete              = true
+
+  vpc_zone_identifier       = var.subnet_ids
+
+  launch_template {
+    id      = aws_launch_template.main.id
+    version = "$Latest"
+  }
+
+  dynamic "tag" {
+    for_each = local.all_tags
+    content {
+      content = tag.value.key
+      value = tag.value.value
+      propagate_at_launch = true
+    }
+  }
+}
+
+#  tag {
+#    key                 = "foo"
+#    value               = "bar"
+#    propagate_at_launch = true
+#  }
+#
+#
+#
+#  tag {
+#    key                 = "lorem"
+#    value               = "ipsum"
+#    propagate_at_launch = false
+#  }
--- var.tf
+++ var.tf
@@ -5,3 +5,7 @@ variable "allow_cidr" {}
 
 variable "component" {}
 variable "app_port" {}
+variable "max_size" {}
+variable "min_size" {}
+variable "desired_capacity" {}
+variable "instance_type" {}
