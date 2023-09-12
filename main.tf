fdd1fb0a2fd434023ecfe31c1f0fe6e0de174352
gaddamrk <raj>
2023-09-12 14:30:09 +0530

    jr

--- main.tf
+++ main.tf
@@ -43,7 +43,10 @@ resource "aws_iam_policy" "policy" {
           "ssm:GetParameters",
           "ssm:GetParameter"
         ],
-        "Resource": "arn:aws:ssm:us-east-1:650202217229:parameter/${var.env}.${var.component}*"
+        "Resource": [
+          "arn:aws:ssm:us-east-1:650202217229:parameter/${var.env}.${var.component}*",
+          "arn:aws:ssm:us-east-1:650202217229:parameter/nexus*"
+        ]
       },
       {
         "Sid": "VisualEditor1",
