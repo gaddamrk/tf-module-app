b166bcb9ccafc9a4f555aab75893954f35b417bc
gaddamrk <raj>
2023-09-13 12:49:52 +0530

    jr

--- user_data.sh
+++ user_data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 labauto ansible
-ansible-pull -i localhost, -U https://github.com/gaddamrk/roboshop-ansible roboshop.yml -e ROLE_NAME=frontend -e env=dev | tee /opt/ansible.log
+ansible-pull -i localhost, -U https://github.com/gaddamrk/roboshop-ansible roboshop.yml -e ROLE_NAME=${component} -e env=${env} | tee /opt/ansible.log
