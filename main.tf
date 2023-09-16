baa67990dd9a036f908be3c93297c557de94b528
gaddamrk <raj>
2023-09-16 06:55:51 +0530

    jr

--- main.tf
+++ main.tf
@@ -45,7 +45,8 @@ resource "aws_iam_policy" "policy" {
         ],
         "Resource": [
           "arn:aws:ssm:us-east-1:650202217229:parameter/${var.env}.${var.component}*",
-          "arn:aws:ssm:us-east-1:650202217229:parameter/nexus*"
+          "arn:aws:ssm:us-east-1:650202217229:parameter/nexus*",
+          "arn:aws:ssm:us-east-1:650202217229:parameter/${var.env}.docdb*"
         ]
       },
       {
